9d30ec3143789b7d5779095a0c60c9a5359c9a2e
josedabh <josedabh@gmail.com>
2025-03-27 22:01:52 +0100

    Fix the problem

--- src/main/java/com/rq/manager/authusers/service/AuthService.java
+++ src/main/java/com/rq/manager/authusers/service/AuthService.java
@@ -1,7 +1,5 @@
 package com.rq.manager.authusers.service;
 
-import java.util.UUID;
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -76,15 +74,4 @@ public class AuthService {
 	    return jwtUtil.generarToken(authentication);
 	}
 
-	
-	
-	
-	//cONTROLAR EL TIEMPOO QUE TENDRA DE PREMIUM 
-	//O SI NO NO TIENE SENTIDO EL METODO
-	public User payPremium(UUID id) {
-		User user = userRepository.findById(id)
-				.orElseThrow(() -> new NullPointerException());
-		user.setRol(Rol.PREMIUM);
-		return user;
-	}
 }
