aea721563b4bc2c5d107bbe23947b97d9e434659
josedabh <josedabh@gmail.com>
2025-06-10 19:57:40 +0200

    Fix repository reward

--- src/main/java/com/rq/manager/authusers/repository/RewardRepository.java
+++ src/main/java/com/rq/manager/authusers/repository/RewardRepository.java
@@ -1,15 +1,9 @@
 package com.rq.manager.authusers.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.rq.manager.authusers.entity.Reward;
 
 public interface RewardRepository extends JpaRepository<Reward, Long> {
 
-    @Query(value = "SELECT * FROM REWARD R WHERE R.VISIBLE = TRUE",
-            nativeQuery = true)
-    List<Reward> findAllVisible();
 }
